2c86f786d7df0dfa31b669c6bdc5fce0404f112f
Hamlix <shirohamlix@gmail.com>
2018-01-07 19:12:30 +0100

    Ajout de la partie SQL

--- src/main/Mediateur.java
+++ src/main/Mediateur.java
@@ -2,6 +2,21 @@ package main;
 
 public class Mediateur {
     static void main(String[] args){
+        /* SQL */
+        Extractor_SQL e_sql = new Extractor_SQL();
+        Boolean connexion_sql = null;
+        int res_sql[][] = new int[3][3];
+
+        connexion_sql=e_sql.connexion_BDD();
+        if(connexion_sql){
+            System.out.println("La connexion à la base de données : REUSSI ! ");
+            res_sql[0][0] = e_sql.requete_1();
+            res_sql[1][0] = e_sql.requete_2();
+            res_sql[2] = e_sql.requete_3();
+            connexion_sql = e_sql.deconnexion_BDD();
+            if(!connexion_sql)
+                System.out.println("Déconnexion de la base données : REUSSI !");
+        }
 
     }
 }
