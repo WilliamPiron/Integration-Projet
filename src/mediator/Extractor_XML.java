b25c0343754abb06f2e837bda2a402065e1b03fa
Hamlix <shirohamlix@gmail.com>
2018-01-07 20:17:52 +0100

    MAJ du médiator

--- src/mediator/Extractor_XML.java
+++ src/mediator/Extractor_XML.java
@@ -2,4 +2,16 @@ package mediator;
 
 public class Extractor_XML {
 
+    public int[] requete_1(){
+        return null;
+    }
+
+    public int requete_2(){
+        return 0;
+    }
+
+    public int[] requete_3(){
+        return null;
+    }
+
 }
